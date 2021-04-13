c36bbe472da42bc53c8d0d8fe48c9ceb4b35992a
Marvin Muiruri <46806806+Madevalo@users.noreply.github.com>
2021-04-13 19:45:10 +0300

    Update BookCategories.php

--- app/Models/BookCategories.php
+++ app/Models/BookCategories.php
@@ -1,12 +1,3 @@
 <?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-use app/Models/Issue.php;
-
-class BookCategories extends Model
-{
-    protected $fillable = ['category'];
-}
 ?>
